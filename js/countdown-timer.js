8f725df0dd29229b5bde4e653f227fe0c3564569
Vungsovanreach Kong <vungsovanreachkong@Vungsovanreachs-iMac.local>
2021-10-18 11:30:14 +0900

    engage day

---
+++ js/countdown-timer.js
@@ -0,0 +1,26 @@
+function countdown() {
+    var now = new Date();
+
+    // This is where you set the date to count down to in the format (year, month, day).
+    var eventDate = new Date(2021, 9, 18, 9);
+    var currentTime = now.getTime();
+    var eventTime = eventDate.getTime();
+    var remTime = currentTime - eventTime;
+
+    var s = Math.floor(remTime / 1000);
+    var m = Math.floor(s / 60);
+    var h = Math.floor(m / 60);
+    var d = Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    document.getElementById("days").textContent = d;
+    document.getElementById("hours").textContent = h;
+    document.getElementById("minutes").textContent = m;
+    document.getElementById("seconds").textContent = s;
+
+    setTimeout(countdown, 1000);
+}
+countdown();
